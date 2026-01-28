acfc3697882a850da65350e3131667c9ca86d86e
Anjali Vadukar <anjaliprajapati2636@gmail.com>
2026-01-28 13:10:01 +0530

    [A] [variables & datatypes files are added]

---
+++ datatypes.js
@@ -0,0 +1,145 @@
+//two types of data types
+//1.primitive
+ //Number
+ //BigInt
+ //String
+ //Boolean
+ //Symbol
+ //null
+ //undefined
+
+//2.Non primitive
+ //Array
+ //Object
+ //function
+
+ //let num=1;
+ //let num2=2;
+// console.log("num",num,num2);
+
+//let name="Anjali";
+//let name2="Yashvi";
+//console.log("name",name,name2)
+
+//Boolean
+
+//let isTrue=true;
+//let isFalse=false;
+//console.log("boolean:", isFalse)
+
+//undefined
+
+//let a;
+//console.log("undefined:",a)
+
+//BigInt
+
+//let BigInt=232434638732904738564375n;
+//console.log(BigInt);
+
+//2. Non primitive
+
+//object
+
+//let student={
+    //name:"anjali",
+    //roll:45,
+    //address:{
+     //   city:"ksd",
+    //}
+//}
+//console.log(student);
+
+//array
+
+//let a=[1,2,3,4,5,6,7,8,9]
+//let student={
+
+//}
+//let arrayOFstud=[{},{}]
+//console.log(a);
+//array of object
+
+//function
+
+//function greet(){
+//    console.log("this is function");
+//}
+//greet();
+
+//parameterized function
+//function showValues(value)
+//{
+//       console.log("this is value:",value);
+//       if(value == 20){
+//        console.log("number-----");
+//       }
+//}
+//showValues();
+//showValues(20);
+//showValues([1,2,3,4,5,6]);
+
+//function arithmetic(num1,num2){
+//    sum=num1+num2;
+//    sub=num1-num2;
+//    multi=num1*num2;
+//    div=num1/num2
+//    console.log("SUM=",sum,"SUB=",sub,"MULTIPLICATION=",multi,"Divison=",div);
+//}
+//arithmetic(20,100);
+
+//const add=(num1,num2)=>{
+//     return num1+num2
+//}
+//const sub=(num1,num2)=>{
+//     return num1-num2
+//}
+//const multy=(num1,num2)=>{
+//     return num1*num2
+//}
+//const div=(num1,num2)=>{
+//     return num1/num2
+//}
+//console.log("this is arrow fun. :",add(12,12))
+//console.log("this is arrow fun. :",sub(122,12))
+//console.log("this is arrow fun. :",multy(12,12))
+//console.log("this is arrow fun. :",div(48,12))
+
+//let value=10;
+//console.log("Default Value:",value);
+
+//value+=5///value + 5
+//console.log(value)
+
+//value-=5///value - 5
+//console.log(value)
+
+//value*=5///value * 5
+//console.log(value)
+
+//value/=5///value / 5
+//console.log(value)
+
+//let age=20;
+//let hasId=true;
+//let name="React"
+//let obj={
+//    name:'demo',
+//    roll:20
+//}
+
+//console.log("logical AND (&&):",age>18 && hasId);
+//console.log("logical OR (||):",age>18 || hasId);
+//console.log("logical NOT (!)):", !hasId);
+
+//console.log(typeof age)
+//console.log(typeof hasId)
+//console.log(typeof name)
+//console.log(typeof obj)
+
+let value=10;
+let num="10"
+
+console.log("valur ==num :",value == num);
+console.log("valur ===num :",value === num);
+console.log("valur !=num :",value != num);
---
+++ variable.js
@@ -0,0 +1,10 @@
+//var a=10;
+//var a=30;
+//console.log(a);
+
+//let
+let a=20;
+console.log(a);
+//const
+const pi=3.14;
+console.log(pi);
